d8faff7838fb4e954da222825a0de9641998731b
grettygoose <gheinric@fredhutch.org>
2018-08-15 15:04:23 -0700

    mock json

--- src/app/dataset.ts
+++ src/app/dataset.ts
@@ -1,5 +1,8 @@
 export class Dataset {
   id: number;
   name: string;
+  columns: number;
+  public: string;
+  contact: string;
 }
 
--- src/app/mock-datasets.ts
+++ src/app/mock-datasets.ts
@@ -1,14 +1,15 @@
 import { Dataset } from './dataset';
 
 export const DATASETS: Dataset[] = [
-  { id: 11, name: 'Breast' },
-  { id: 12, name: 'Brain' },
-  { id: 13, name: 'Lung' },
-  { id: 14, name: 'Neck' },
-  { id: 15, name: 'Broad' },
-  { id: 16, name: 'Sarc' },
-  { id: 17, name: 'Prostate' },
-  { id: 18, name: 'Giloma' },
-  { id: 19, name: 'Ovarian' },
-  { id: 20, name: 'Skin' }
+  { id: 11, name: 'Breast', columns: 25, public: 'No', contact: 'blah@yahoo.com' },
+  { id: 12, name: 'Brain', columns: 12, public: 'No', contact: 'cat@yahoo.com'  },
+  { id: 13, name: 'Lung', columns: 35, public: 'Yes', contact: 'dog@yahoo.com'  },
+  { id: 14, name: 'Neck', columns: 45 , public: 'Yes' , contact: 'fish@yahoo.com'  },
+  { id: 15, name: 'Broad', columns: 29 , public: 'Yes', contact: 'donkey@yahoo.com'  },
+  { id: 16, name: 'Sarc', columns: 35, public: 'Yes' , contact: 'bird@yahoo.com'  },
+  { id: 17, name: 'Prostate', columns: 67, public: 'Yes', contact: 'monkey@yahoo.com'   },
+  { id: 18, name: 'Giloma', columns: 89, public: 'Yes' , contact: 'zebra@yahoo.com'  },
+  { id: 19, name: 'Ovarian', columns: 12, public: 'No' , contact: 'elephant@yahoo.com'  },
+  { id: 20, name: 'Skin', columns: 32, public: 'Yes' , contact: 'rabbit@yahoo.com'  }
 ];
+
